8451e25638eb21c44e4483a0c1d9bf14f4e2c32c
aditi <upadhyayaditi759@gmail.com>
2025-11-12 11:09:19 +0530

    implemented hero section

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import NavBar from "./components/NavBar.jsx"
+import Hero from "./components/Hero.jsx";
+
+const App = () => {
+    return (
+        <main>
+            <NavBar />
+            <Hero />
+        </main>
+    )
+}
+export default App
