790696903c5f5943dd6b65235d627fbc6c84d519
Aditi Upadhyay <au.aditi08@gmail.com>
2025-11-12 16:23:23 +0530

    implemented product view

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -1,6 +1,6 @@
 import useMacbookStore from "../store/index.jsx";
 import clsx from "clsx"
-import {Canvas} from "@react-three/fiber"\
+import {Canvas} from "@react-three/fiber"
 import {Box, OrbitControls} from "@react-three/drei";
 
 const ProductViewer = () => {
@@ -46,7 +46,7 @@ const ProductViewer = () => {
             </div>
             <Canvas id="canvas" camera={{ position : [0, 2, 5], fov: 50, near: 0.1, far: 100}}  >
                 <Box position={[0, 0, 0]} scale={10 * scale} material-color={color} />
-                <OrbitControls enableZoom={false}  />
+                <OrbitControls enableZoom={false}  /
             </Canvas>
         </section>
     )
