74c28b06a670927ce41ab34f575b53ffef52f266
Aditi Upadhyay <au.aditi08@gmail.com>
2025-11-27 17:05:32 +0530

    implement the color switcher for the model

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -17,7 +17,7 @@ const ProductViewer = () => {
             <h2>Take a closer look.</h2>
             <div className="controls">
 
-                <p className="info">MacbookPro {scale} in {color}</p>
+                {/*<p className="info">MacbookPro {scale} in {color}</p>*/}
 
                 <div className="flex-center gap-5 mt-5">
                     <div className="color-control">
