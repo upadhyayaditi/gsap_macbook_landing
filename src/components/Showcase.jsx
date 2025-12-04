94aa78610f43a112689971a894dcb651f89b4772
Aditi Upadhyay <au.aditi08@gmail.com>
2025-12-04 17:01:29 +0530

    implement showcase section

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -3,7 +3,7 @@ import {useGSAP} from "@gsap/react";
 import gsap from 'gsap';
 
 const Showcase = () => {
-    const isTablet = useMediaQuery({ query: '(max-width: 1024px)'});
+    const isTablet = useMediaQuery({ query: '(max-width: 1024px)'})
 
     useGSAP(() => {
         if(!isTablet) {
